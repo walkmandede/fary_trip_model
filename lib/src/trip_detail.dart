9a55c764fb24f6543ec9f913e71cb815c8a08cc2
ame <aungmo1020@gmail.com>
2024-12-13 15:38:59 +0630

    null check in faryDiscount

--- lib/src/trip_detail.dart
+++ lib/src/trip_detail.dart
@@ -71,8 +71,8 @@ class FaryTripDetail {
                   value: tripMeta['faryDiscountValue'] ?? 0,
                   code: 'faryDiscount',
                   discountType: DiscountType.values.firstWhere(
-                    (element) => element.name == tripMeta['faryDiscountType'],
-                  ),
+                      (element) => element.name == tripMeta['faryDiscountType'],
+                      orElse: () => DiscountType.mmk),
                   promotionType: PromotionType.discount,
                   b2bType: 'faryDiscount'),
           startDateTime:
